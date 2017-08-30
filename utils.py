ea27606ea5a2264b9dfafc8d87c4c3f759b46b63
Victor Ananyev <vindex10@gmail.com>
2017-08-30 15:23:38 +0300

    clean utils

--- utils.py
+++ utils.py
@@ -1,7 +1,4 @@
-from types import ModuleType
 from timeit import default_timer as timer
-import os
-import re
 
 class timing:
     def __enter__(self):
@@ -11,11 +8,3 @@ class timing:
     def __exit__(self, type, value, traceback):
         pass
 
-# mark as deprecated
-def moduleVars(mod):
-    return {k: v for k,v in mod.__dict__.items()
-            if k in dir(mod)
-            and not k.startswith("__")
-            and not k.startswith("_")
-            and not callable(mod.__dict__[k])
-            and not isinstance(mod.__dict__[k], ModuleType)}
